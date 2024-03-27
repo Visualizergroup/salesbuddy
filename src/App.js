3b0b1e723468bb3ecf470d5c51e12b02efe46934
VisualSeb <124244112+VisualSeb@users.noreply.github.com>
2024-03-27 11:01:32 +0200

    Revert "Revert "added changes to inpaint""

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
+import Inpaint from "./Components/Inpaint";
 
 class App extends Component {
   constructor(props) {
